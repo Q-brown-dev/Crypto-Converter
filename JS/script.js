1990a9e93b056489da57739e82945d797fd37b02
Q-brown-dev <anyikwabruno1@gmail.com>
2025-04-11 14:52:48 +0100

    add all files and folders

---
+++ JS/script.js
@@ -0,0 +1,161 @@
+const number1 = document.getElementById("Bit");
+const number2 = document.getElementById("BitNum");
+const Output = document.getElementById("Out");
+const mybutton = document.getElementById("btn");
+
+if (mybutton) {
+
+    let Result = 0;
+    mybutton.addEventListener("click", function BitConverter() {
+        const number1Int = parseInt(number1.value);
+        const number2Int = parseInt(number2.value);
+        Result = number1Int * number2Int;
+        Output.textContent = Result.toString();
+    });
+}
+
+const Ethrum = document.getElementById("Ethrum");
+const EthtumNum = document.getElementById("EthtumNum");
+const Output2 = document.getElementById("Out2");
+const mybutton2 = document.getElementById("btn2");
+
+if (mybutton2) {
+
+    let Result2 = 0;
+    mybutton2.addEventListener("click", function EthrumConverter() {
+        const number1Int = parseInt(Ethrum.value);
+        const number2Int = parseInt(EthtumNum.value);
+        Result2 = number1Int * number2Int;
+        Output2.textContent = Result2.toString();
+    });
+}
+
+
+const BNB = document.getElementById("BNB");
+const BNBNum = document.getElementById("BNBNum");
+const Output3 = document.getElementById("Out3");
+const mybutton3 = document.getElementById("btn3");
+
+if (mybutton3) {
+    let Result3 = 0;
+    mybutton3.addEventListener("click",function BNBConverter() {
+        const number1Int = parseInt(BNB.value);
+        const number2Int = parseInt(BNBNum.value);
+        Result3 = number1Int * number2Int;
+        Output3.textContent = Result3.toString();
+    });
+}
+
+
+const  Dogein = document.getElementById("Dogein");
+const DogeinNum = document.getElementById("DogeinNum");
+const Output4 = document.getElementById("Out4");
+const mybutton4 = document.getElementById("btn4");
+
+if (mybutton4) {
+    let Result4 = 0;
+    mybutton4.addEventListener("click", function DogeinConverter() {
+        const number1Int = parseInt(Dogein.value);
+        const number2Int = parseInt(DogeinNum.value);
+        Result4 = number1Int * number2Int;
+        Output4.textContent = Result4.toString();
+    });
+}
+
+const  Slaner = document.getElementById("Slaner");
+const SlanerNum = document.getElementById("SlanerNum");
+const Output5 = document.getElementById("Out5");
+const mybutton5 = document.getElementById("btn5");
+
+if (mybutton5) {
+    let Result5 = 0;
+    mybutton5.addEventListener("click", function SlannerConverter() {
+        const number1Int = parseInt(Slaner.value);
+        const number2Int = parseInt(SlanerNum.value);
+        Result5 = number1Int * number2Int;
+        Output5.textContent = Result5.toString();
+    });
+}
+
+
+const  Teher = document.getElementById("Teher");
+const TetherNum = document.getElementById("TetherNum");
+const Output6 = document.getElementById("Out6");
+const mybutton6 = document.getElementById("btn6");
+
+if (mybutton6) {
+    let Result6 = 0;
+    mybutton6.addEventListener("click", function TetherConverter() {
+        const number1Int = parseInt(Teher.value);
+        const number2Int = parseInt(TetherNum.value);
+        Result6 = number1Int * number2Int;
+        Output6.textContent = Result6.toString();
+    });
+}
+
+
+const  PI = document.getElementById("PI");
+const PINum = document.getElementById("PINum");
+const Output7 = document.getElementById("Out7");
+const mybutton7 = document.getElementById("btn7");
+
+if (mybutton7) {
+    let Result7 = 0;
+    mybutton7.addEventListener("click", function TetherConverter() {
+        const number1Int = parseInt(PI.value);
+        const number2Int = parseInt(PINum.value);
+        Result7 = number1Int * number2Int;
+        Output7.textContent = Result7.toString();
+    });
+}
+
+const  POLKADOT = document.getElementById("POLKADOT");
+const POLKADOTNUM = document.getElementById("POLKADOTNUM");
+const Output8 = document.getElementById("Out8");
+const mybutton8 = document.getElementById("btn8");
+
+if (mybutton8) {
+    let Result8 = 0;
+    mybutton8.addEventListener("click", function CADANERConverter() {
+        const number1Int = parseInt(POLKADOT.value);
+        const number2Int = parseInt(POLKADOTNUM.value);
+        Result8 = number1Int * number2Int;
+        Output8.textContent = Result8.toString();
+    });
+}
+
+
+const  XRP = document.getElementById("XRP");
+const XRPNum = document.getElementById("XRPNum");
+const Output9 = document.getElementById("Outtwo");
+const mybutton9 = document.getElementById("btntwo");
+
+if (mybutton9) {
+    let Result9 = 0;
+    mybutton9.addEventListener("click", function XRPConverter() {
+        const number1Int = parseInt(XRP.value);
+        const number2Int = parseInt(XRPNum.value);
+        Result9 = number1Int * number2Int;
+        Output9.textContent = Result9.toString();
+    });
+}
+
+
+
+
+
+
+const  AVAX = document.getElementById("AVAX");
+const AVAXNum = document.getElementById("AVAXNum");
+const Output10 = document.getElementById("Out10");
+const mybutton10 = document.getElementById("btn10");
+
+if (mybutton10) {
+    let Result10 = 0;
+    mybutton10.addEventListener("click", function XRPConverter() {
+        const number1Int = parseInt(AVAX.value);
+        const number2Int = parseInt(AVAXNum.value);
+        Result10 = number1Int * number2Int;
+        Output10.textContent = Result10.toString();
+    });
+}
---
+++ Readme.md
@@ -0,0 +1,16 @@
+### Crypto converter
+#### Made by bruno
+### A Crypto converter  website
+####  description
+* _It is a Crypto converter Web where people can Use in converting their crypto currency
+### Technologies used
+* _ Html_
+* _Gitbash_
+* _css_
+*_JAVASCRIPT_
+### Known bugs 
+* _No known bubs_
+#### License
+* _MIT_
+##### Contact
+* _Anyikwabruno@gmail.com_
